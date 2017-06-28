c147c36b0d6aa27797d0585c3f1a750c5b022a5a
Alan Scarpa <alan.scarpa@gmail.com>
2017-06-28 16:45:53 -0700

    creating user without other info

--- Thirty/Thirty/View Controllers/Signup/SignupViewController.swift
+++ Thirty/Thirty/View Controllers/Signup/SignupViewController.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import Firebase
+import SVProgressHUD
 
-class SignupViewController: UIViewController {
+class SignupViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,5 +26,65 @@ class SignupViewController: UIViewController {
         super.viewWillAppear(animated)
         RootViewController.shared.showNavigationBar = true
     }
-
+    
+    // MARK: - UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let nextTag = textField.tag + 1
+        if let nextResponder = textField.superview?.viewWithTag(nextTag) {
+            nextResponder.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+            signupButtonTapped()
+        }
+        return false
+    }
+    
+    // MARK: - UIResponder
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        let touch = event?.allTouches?.first
+        if touch?.view?.isKind(of: UITextField.self) == false {
+            view.endEditing(true)
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func signupButtonTapped() {
+        guard let credentials = validSignupCredentials() else {
+            present(UIAlertController.createSimpleAlert(withTitle: "Problem Signing Up", message: "Make sure you've filled out all fields correctly, your password has at least 6 characters, and both passwords match.  Please try again!"), animated: true, completion: nil)
+            return
+        }
+        SVProgressHUD.show()
+        FIRAuth.auth()?.createUser(withEmail: credentials.email, password: credentials.password) { (user, error) in
+            if let error = error {
+                SVProgressHUD.dismiss()
+                self.present(UIAlertController.createSimpleAlert(withTitle: "Error", message: error.localizedDescription), animated: true, completion: nil)
+            } else {
+                FIRAuth.auth()?.signIn(withEmail: credentials.email, password: credentials.password, completion: { (user, error) in
+                    SVProgressHUD.dismiss()
+                    if let error = error {
+                        self.present(UIAlertController.createSimpleAlert(withTitle: "Error", message: error.localizedDescription), animated: true, completion: nil)
+                    } else {
+                        RootViewController.shared.goToHomeVC()
+                    }
+                })
+            }
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    private func validSignupCredentials() -> (email: String, password: String, username: String, phoneNumber: String)? {
+        if let email = emailTextField.text,
+            let username = usernameTextField.text,
+            let phoneNumber = phoneNumberTextField.text,
+            let password = passwordTextField.text, password.characters.count >= 6, passwordTextField.text == confirmPasswordTextField.text {
+            return (email, password, username, phoneNumber)
+        }
+        return nil
+    }
+    
 }
