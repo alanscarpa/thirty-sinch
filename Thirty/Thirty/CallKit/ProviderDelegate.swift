b3561f33c230bf8e1c5b27890c34c4028befaa21
Alan Scarpa <alan.scarpa@gmail.com>
2017-10-03 17:06:41 -0700

    setting up call screen

---
+++ Thirty/Thirty/CallKit/ProviderDelegate.swift
@@ -0,0 +1,68 @@
+//
+//  ProviderDelegate.swift
+//  Thirty
+//
+//  Created by Alan Scarpa on 10/3/17.
+//  Copyright © 2017 Thirty. All rights reserved.
+//
+
+import AVFoundation
+import CallKit
+
+class ProviderDelegate: NSObject {
+    // 1.
+    fileprivate let callManager: CallManager
+    fileprivate let provider: CXProvider
+    
+    init(callManager: CallManager) {
+        self.callManager = callManager
+        // 2.
+        provider = CXProvider(configuration: type(of: self).providerConfiguration)
+        
+        super.init()
+        // 3.
+        provider.setDelegate(self, queue: nil)
+    }
+    
+    // 4.
+    static var providerConfiguration: CXProviderConfiguration {
+        let providerConfiguration = CXProviderConfiguration(localizedName: "Hotline")
+        
+        providerConfiguration.supportsVideo = true
+        providerConfiguration.maximumCallsPerCallGroup = 1
+        providerConfiguration.supportedHandleTypes = [.phoneNumber]
+        
+        return providerConfiguration
+    }
+    
+    func reportIncomingCall(uuid: UUID, handle: String, hasVideo: Bool = false, completion: ((NSError?) -> Void)?) {
+        // 1.
+        let update = CXCallUpdate()
+        update.remoteHandle = CXHandle(type: .phoneNumber, value: handle)
+        update.hasVideo = hasVideo
+        
+        // 2.
+        provider.reportNewIncomingCall(with: uuid, update: update) { error in
+            if error == nil {
+                // 3.
+                let call = Call(uuid: uuid, handle: handle)
+                self.callManager.add(call: call)
+            }
+            
+            // 4.
+            completion?(error as? NSError)
+        }
+    }
+}
+
+extension ProviderDelegate: CXProviderDelegate {
+    func providerDidReset(_ provider: CXProvider) {
+        stopAudio()
+        
+        for call in callManager.calls {
+            call.end()
+        }
+        
+        callManager.removeAllCalls()
+    }
+}
