6fa1b48424cfc8d062be210031076eb137d78c12
Alan Scarpa <alan.scarpa@gmail.com>
2018-01-17 00:35:14 -0800

    cleaned up app delegate

--- Thirty/Thirty/AppDelegate.swift
+++ Thirty/Thirty/AppDelegate.swift
@@ -51,28 +51,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     
     func setUpRemoteNotificationsForApplication(_ application: UIApplication) {
         if !application.isRegisteredForRemoteNotifications {
-            if #available(iOS 10.0, *) {
-                let center = UNUserNotificationCenter.current()
-                center.delegate = self
-                center.requestAuthorization(options: [.badge, .sound, .alert], completionHandler: { (granted, error) in
-                    if granted {
-                        application.registerForRemoteNotifications()
-                    } else {
-                        // TODO: Present screen asking to turn on notifications
-                    }
-                })
-            } else {
-                let notificationSettings = UIUserNotificationSettings(types: [.badge, .alert, .sound], categories: nil)
-                application.registerUserNotificationSettings(notificationSettings)
-            }
-        }
-    }
-    
-    func application(_ application: UIApplication, didRegister notificationSettings: UIUserNotificationSettings) {
-        if notificationSettings.types != .none {
-            application.registerForRemoteNotifications()
-        } else {
-            // TODO: Present screen asking to turn on notifications
+            let center = UNUserNotificationCenter.current()
+            center.delegate = self
+            center.requestAuthorization(options: [.badge, .sound, .alert], completionHandler: { (granted, error) in
+                if granted {
+                    application.registerForRemoteNotifications()
+                } else {
+                    // TODO: Present screen asking to turn on notifications
+                }
+            })
         }
     }
     
@@ -88,26 +75,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         SinchManager.shared.push?.application(application, didReceiveRemoteNotification: userInfo)
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
     func applicationWillEnterForeground(_ application: UIApplication) {
+        print("entering foreground")
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
 }
