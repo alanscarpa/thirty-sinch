fbcfc53818fe51482336fa95183dcf88010be76d
Alan Scarpa <alan.scarpa@gmail.com>
2018-03-08 13:02:00 -0800

    upgraded to twilio 2.0 beta

--- Thirty/Thirty/View Controllers/Call/CallViewController.swift
+++ Thirty/Thirty/View Controllers/Call/CallViewController.swift
@@ -11,75 +11,124 @@ import TwilioVideo
 import CallKit
 import Alamofire
 
-class CallViewController: UIViewController, TVIRoomDelegate, TVIParticipantDelegate, TVIVideoViewDelegate {
+class CallViewController: UIViewController, TVIRoomDelegate, TVIRemoteParticipantDelegate, TVIVideoViewDelegate, CXProviderDelegate {
+    
+    // MARK: -
+    func providerDidReset(_ provider: CXProvider) {
+        //
+    }
+    
 
-    @IBOutlet weak var remoteVideoView: UIView!
-    @IBOutlet weak var localVideoView: UIView!
+    @IBOutlet weak var remoteVideoView: TVIVideoView!
+    @IBOutlet weak var localVideoView: TVIVideoView!
     @IBOutlet weak var remoteUserLabel: UILabel!
     @IBOutlet weak var answerCallButton: UIButton!
     @IBOutlet weak var timeRemainingLabel: UILabel!
     
-    // This will be the room name
+    // This will also be the room name
     var caller = ""
     var timer = Timer()
-    
+    var tokenGeneratorAddress = "https://php-ios.herokuapp.com/token.php"
     /**
      * We will create an audio device and manage it's lifecycle in response to CallKit events.
      */
-
     var room: TVIRoom?
     var camera: TVICameraCapturer?
-    
+    /**
+     * We will create an audio device and manage it's lifecycle in response to CallKit events.
+     */
+    var audioDevice: TVIDefaultAudioDevice = TVIDefaultAudioDevice()
     // Create an audio track
     var localAudioTrack = TVILocalAudioTrack()
-    
     // Create a Capturer to provide content for the video track
     var localVideoTrack : TVILocalVideoTrack?
+    var remoteParticipant: TVIRemoteParticipant?
     
-    var remoteParticipant: TVIParticipant?
-    var remoteView: TVIVideoView?
+    // CallKit components
+    let callKitProvider: CXProvider
+    let callKitCallController: CXCallController
+    var callKitCompletionHandler: ((Bool)->Swift.Void?)? = nil
 
     
     // MARK: - View Lifecycle
     
+    required init?(coder aDecoder: NSCoder) {
+        let configuration = CXProviderConfiguration(localizedName: "CallKit Quickstart")
+        configuration.maximumCallGroups = 1
+        configuration.maximumCallsPerCallGroup = 1
+        configuration.supportsVideo = true
+        if let callKitIcon = UIImage(named: "callkitIcon") {
+            configuration.iconTemplateImageData = UIImagePNGRepresentation(callKitIcon)
+        }
+        
+        callKitProvider = CXProvider(configuration: configuration)
+        callKitCallController = CXCallController()
+        
+        super.init(coder: aDecoder)
+        
+        callKitProvider.setDelegate(self, queue: nil)
+    }
+    
+    deinit {
+        // CallKit has an odd API contract where the developer must call invalidate or the CXProvider is leaked.
+        callKitProvider.invalidate()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startLocalPreviewVideo()
+        connectToRoom()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
+    // MARK: - Setup
+    
+    private func startLocalPreviewVideo() {
         // Create a video track with the capturer.
         if let camera = TVICameraCapturer(source: .frontCamera),
             let videoTrack = TVILocalVideoTrack(capturer: camera) {
-            // TVIVideoView is a TVIVideoRenderer and can be added to any TVIVideoTrack.
-            let renderer = TVIVideoView(frame: CGRect(x: localVideoView.frame.origin.x, y: localVideoView.frame.origin.y - 25, width: 350, height: 300))
-            renderer.contentMode = .scaleAspectFit
-            // Add renderer to the video track
-            videoTrack.addRenderer(renderer)
-            self.localVideoTrack = videoTrack
+            videoTrack.addRenderer(localVideoView)
+            localVideoTrack = videoTrack
             self.camera = camera
-            self.view.addSubview(renderer)
-            renderer.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.flipCamera)))
+            localVideoView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.flipCamera)))
         } else {
             print("Couldn't create TVICameraCapturer or TVILocalVideoTrack")
         }
-        
-        let parameters: Parameters = ["identity": UserManager.shared.currentUserUsername!, "room": caller]
-        Alamofire.request("https://php-ios.herokuapp.com/token.php", parameters: parameters).response { [weak self] response in
-            if let data = response.data, let accessToken = String(data: data, encoding: .utf8) {
-                // Create room
-                let connectOptions = TVIConnectOptions.init(token: accessToken) { [weak self] builder in
-                    builder.roomName = self?.caller
-                    // Will share audio with users in room
-                    if let audioTrack = self?.localAudioTrack {
-                        builder.audioTracks = [audioTrack]
-                    }
-                    // Will share video with users in room
-                    if let videoTrack = self?.localVideoTrack {
-                        builder.videoTracks = [videoTrack]
-                    }
-                }
-                self?.room = TwilioVideo.connect(with: connectOptions, delegate: self)
+    }
+    
+    private func connectToRoom() {
+        let currentUsername = UserManager.shared.currentUserUsername!
+        let parameters: Parameters = ["identity": currentUsername, "room": caller]
+        // Generate access token
+        Alamofire.request(tokenGeneratorAddress, parameters: parameters).validate().response { [weak self] response in
+            if let error = response.error {
+                let alertVC = UIAlertController.createSimpleAlert(withTitle: "Error", message: "Unable to generate access token.  \(error.localizedDescription)")
+                self?.present(alertVC, animated: true, completion: nil)
+            } else if let data = response.data, let accessToken = String(data: data, encoding: .utf8) {
+                // Create room and connect
+                let connectOptions = self?.defaultConnectOptionsWithAccessToken(accessToken)
+                self?.room = TwilioVideo.connect(with: connectOptions!, delegate: self)
             }
         }
-
-        
+    }
+    
+    private func defaultConnectOptionsWithAccessToken(_ accessToken: String) -> TVIConnectOptions {
+        let connectOptions = TVIConnectOptions.init(token: accessToken) { [weak self] builder in
+            builder.roomName = self?.caller
+            // Will share audio with users in room
+            if let audioTrack = self?.localAudioTrack {
+                builder.audioTracks = [audioTrack]
+            }
+            // Will share video with users in room
+            if let videoTrack = self?.localVideoTrack {
+                builder.videoTracks = [videoTrack]
+            }
+        }
+        return connectOptions
     }
     
     // MARK: - TVIRoomDelegate
@@ -90,79 +139,78 @@ class CallViewController: UIViewController, TVIRoomDelegate, TVIParticipantDeleg
         if let localParticipant = room.localParticipant {
             print("Local identity \(localParticipant.identity)")
         }
-        
         // Connected participants
-        let participants = room.participants;
-        print("Number of connected Participants \(participants.count)")
+        let remoteParticipants = room.remoteParticipants
+        print("Number of connected Participants \(remoteParticipants.count)")
+        // Start timer if callee is joining the room and is the 2nd participant
+        if remoteParticipants.count == 1 {
+            answerCall()
+        }
     }
-    
     
     func room(_ room: TVIRoom, didDisconnectWithError error: Error?) {
         print("Disconnected from room \(room.name)")
     }
     
-    func room(_ room: TVIRoom, participantDidConnect participant: TVIParticipant) {
-        print ("Participant \(participant.identity) has joined Room \(room.name)")
-        participant.delegate = self
+    func room(_ room: TVIRoom, participantDidConnect participant: TVIRemoteParticipant) {
+        if (self.remoteParticipant == nil) {
+            self.remoteParticipant = participant
+            self.remoteParticipant?.delegate = self
+        }
+        logMessage(messageText: "Participant \(participant.identity) connected with \(participant.remoteAudioTracks.count) audio and \(participant.remoteVideoTracks.count) video tracks")
         answerCall()
     }
     
-    func room(_ room: TVIRoom, participantDidDisconnect participant: TVIParticipant) {
-        print ("Participant \(participant.identity) has left Room \(room.name)")
+    func room(_ room: TVIRoom, participantDidDisconnect participant: TVIRemoteParticipant) {
+        logMessage(messageText: "Room \(room.name), Participant \(participant.identity) disconnected")
         endCall()
     }
     
-    // MARK: - TVIParticipantDelegate
+    // MARK: - TVIRemoteParticipantDelegate
     
-    /*
-     * In the Participant Delegate, we can respond when the Participant adds a Video
-     * Track by rendering it on screen.
-     */
-    
-    func participant(_ participant: TVIParticipant, addedVideoTrack videoTrack: TVIVideoTrack) {
-        print("Participant \(participant.identity) added video track")
-        remoteView = TVIVideoView(frame: self.view.bounds, delegate: self)
-        videoTrack.addRenderer(remoteView!)
-        view.addSubview(remoteView!)
+    func subscribed(to videoTrack: TVIRemoteVideoTrack,
+                    publication: TVIRemoteVideoTrackPublication,
+                    for participant: TVIRemoteParticipant) {
+        
+        // We are subscribed to the remote Participant's video Track. We will start receiving the
+        // remote Participant's video frames now.
+        
+        logMessage(messageText: "Subscribed to video track for Participant \(participant.identity)")
+        
+        if (remoteParticipant == participant) {
+            videoTrack.addRenderer(remoteVideoView)
+        }
     }
     
-    func participant(_ participant: TVIParticipant, addedAudioTrack audioTrack: TVIAudioTrack) {
-        //
+    func unsubscribed(from videoTrack: TVIRemoteVideoTrack,
+                      publication: TVIRemoteVideoTrackPublication,
+                      for participant: TVIRemoteParticipant) {
+        
+        // We are unsubscribed from the remote Participant's video Track. We will no longer receive the
+        // remote Participant's video.
+        
+        logMessage(messageText: "Unsubscribed from video track for Participant \(participant.identity)")
+        
+        if (remoteParticipant == participant) {
+            videoTrack.removeRenderer(remoteVideoView)
+            remoteVideoView.removeFromSuperview()
+        }
     }
+    
     
     // MARK: - TVIVideoViewDelegate
-    
-    // Lastly, we can subscribe to important events on the VideoView
+        
     func videoView(_ view: TVIVideoView, videoDimensionsDidChange dimensions: CMVideoDimensions) {
         print("The dimensions of the video track changed to: \(dimensions.width)x\(dimensions.height)")
         self.view.setNeedsLayout()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
+    // MARK: - Call Handling
     
     func answerCall() {
         answerCallButton.isHidden = true
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        timer.invalidate()
-    }
-    
-    // MARK: - Actions
-    
-    @IBAction func answerCallButtonTapped() {
-        answerCall()
-    }
-    
-    @IBAction func cancelButtonTapped() {
-        endCall()
-    }
-    
-    // MARK: - Call Handling
     
     @objc func updateTime() {
         guard let timeRemainingString = timeRemainingLabel.text else { return }
@@ -180,7 +228,16 @@ class CallViewController: UIViewController, TVIRoomDelegate, TVIParticipantDeleg
         RootViewController.shared.popViewController()
     }
     
-    // Select between the front and (wide) back camera.
+    // MARK: - Actions
+    
+    @IBAction func answerCallButtonTapped() {
+        answerCall()
+    }
+    
+    @IBAction func cancelButtonTapped() {
+        endCall()
+    }
+    
     @objc func flipCamera() {
         if let camera = camera {
             if (camera.source == .frontCamera) {
@@ -190,4 +247,11 @@ class CallViewController: UIViewController, TVIRoomDelegate, TVIParticipantDeleg
             }
         }
     }
+    
+    // MARK: - Helpers
+    
+    func logMessage(messageText: String) {
+        NSLog(messageText)
+    }
+    
 }
