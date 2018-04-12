22cfad9c3f7b1306a6753518c810e192ae16ae28
Alan Scarpa <alan.scarpa@gmail.com>
2018-04-12 15:33:48 -0700

    add firstname lastname to User model

--- Thirty/Thirty/Models/User.swift
+++ Thirty/Thirty/Models/User.swift
@@ -9,15 +9,22 @@
 import Foundation
 
 class User {
-    var username: String = ""
-    var email: String = ""
-    var phoneNumber: String = ""
-    var password: String = ""
+    var username = ""
+    var email = ""
+    var phoneNumber = ""
+    var password = ""
+    var firstName = ""
+    var lastName = ""
+    var deviceToken: String?
+
     var userNameLowercased: String {
         return username.lowercased()
     }
     var doNotDisturb = false
-    var deviceToken: String?
+
+    var fullName: String {
+        return "\(firstName) \(lastName)"
+    }
     
     convenience init(username: String, email: String, phoneNumber: String, password: String, deviceToken: String?) {
         self.init()
